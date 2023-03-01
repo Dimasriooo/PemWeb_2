2adc8c2a17616c39771b01ed7b3ba044dbc4066f
Dimasriooo <114458950+Dimasriooo@users.noreply.github.com>
2023-03-01 10:38:12 +0700

    PHP Form Processing

---
+++ praktikum2/index2.php
@@ -0,0 +1,16 @@
+<?php
+    $_nama = $_GET['nama']; 
+    $_matkul =$_GET["matkul"];
+    $_uts = $_GET["Uts"];
+    $_uas = $_GET['uas'];
+    $_tugas = $_GET['tugas'];
+
+
+    echo '<br> Nama mahasiswa : ' . $_nama;
+    echo '<br> mata kuliah : ' . $_matkul;
+    echo '<br> nilai uts :' . $_uts;
+    echo '<br> nilai uas :' . $_uas;
+    echo '<br> nilai tugas :' . $_tugas;
+    
+
+?>
